3b9c6109d990c84c4e4fe18ccba4fe9b503f0842
yangsen <yang_sen_ysys@163.com>
2022-07-18 16:40:10 +0800

    optimize Android.mk

--- curl/src/main/jni/Android.mk
+++ curl/src/main/jni/Android.mk
@@ -20,7 +20,7 @@ LOCAL_PATH := $(call my-dir)
 
 #cURL prebuilt
 include $(CLEAR_VARS)
-LOCAL_MODULE := curl-builtstrip
+LOCAL_MODULE := curl-built
 LOCAL_SRC_FILES := \
   $(LOCAL_PATH)/lib/$(TARGET_ARCH_ABI)/libcurl.a
 include $(PREBUILT_STATIC_LIBRARY)
@@ -28,7 +28,6 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 #qn-curl
 include $(CLEAR_VARS)
-LOCAL_LDLIBS := -llog
 LOCAL_MODULE := qn-curl
 
 MY_CPP_LIST := $(wildcard $(LOCAL_PATH)/native/*.cpp)
@@ -39,20 +38,21 @@ LOCAL_SRC_FILES := $(MY_CPP_LIST:$(LOCAL_PATH)/%=%)
 #	$(LOCAL_PATH)/native/curl_configuration.cpp
 
 LOCAL_STATIC_LIBRARIES := curl-built
-COMMON_CFLAGS := -Werror -DANDROID -Os -ffunction-sections -fdata-sections
-
-ifeq ($(TARGET_ARCH),arm)
-  LOCAL_CFLAGS := -mfpu=vfp -mfloat-abi=softfp -fno-short-enums
-endif
-
 
 LOCAL_LDFLAGS += -Wl,--gc-sections
-LOCAL_CPPFLAGS += -Os -ffunction-sections -fdata-sections
 
-LOCAL_CFLAGS += $(COMMON_CFLAGS)
+LOCAL_CPPFLAGS += -Os -std=gnu++0x
+LOCAL_CPPFLAGS += -ffunction-sections -fdata-sections -fvisibility=hidden
+
+LOCAL_CFLAGS := -Werror -DANDROID -Os
+LOCAL_CFLAGS += -ffunction-sections -fdata-sections
+ifeq ($(TARGET_ARCH),arm)
+  LOCAL_CFLAGS += -mfpu=vfp -mfloat-abi=softfp -fno-short-enums
+endif
+
 #LOCAL_LDLIBS := -lz -llog -Wl,-s
-LOCAL_LDLIBS := -lz -llog -Wl
-LOCAL_CPPFLAGS += -std=gnu++0x
+LOCAL_LDLIBS := -lz -Wl -llog
+
 LOCAL_C_INCLUDES += \
   $(NDK_PATH)/platforms/$(TARGET_PLATFORM)/arch-$(TARGET_ARCH)/usr/include \
   $(LOCAL_PATH)/lib/include
